6265504bf6b6e46e9aea7f06b302e32aa5e3b23e
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-02-27 23:39:35 +0800

    Add apt::source sbt

--- czchen_environment/manifests/pkg/apt.pp
+++ czchen_environment/manifests/pkg/apt.pp
@@ -1,3 +1,5 @@
 class czchen_environment::pkg::apt {
     include czchen_environment::pkg::apt::debian
+
+    include czchen_environment::pkg::apt::sbt
 }
---
+++ czchen_environment/manifests/pkg/apt/sbt.pp
@@ -0,0 +1,10 @@
+class czchen_environment::pkg::apt::sbt {
+    apt::source { 'sbt':
+      comment     => 'See http://www.scala-sbt.org/download.html',
+      location    => 'https://dl.bintray.com/sbt/debian',
+      release     => '',
+      repos       => '/',
+      include_src => false,
+      include_deb => true,
+    }
+}
