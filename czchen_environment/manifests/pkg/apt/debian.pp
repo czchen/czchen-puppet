03972e7dfc042699f098ba6b8ed47510aa1ebf62
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-02-28 01:39:18 +0800

    Fix indent

--- czchen_environment/manifests/pkg/apt/debian.pp
+++ czchen_environment/manifests/pkg/apt/debian.pp
@@ -1,19 +1,19 @@
 class czchen_environment::pkg::apt::debian {
     apt::source { 'unstable':
-      comment     => 'Debian unstable',
-      location    => 'http://http.debian.net/debian/',
-      release     => 'unstable',
-      repos       => 'main contrib non-free',
-      include_src => true,
-      include_deb => true,
+        comment     => 'Debian unstable',
+        location    => 'http://http.debian.net/debian/',
+        release     => 'unstable',
+        repos       => 'main contrib non-free',
+        include_src => true,
+        include_deb => true,
     }
 
     apt::source { 'experimental':
-      comment     => 'Debian experimental',
-      location    => 'http://http.debian.net/debian/',
-      release     => 'experimental',
-      repos       => 'main contrib non-free',
-      include_src => true,
-      include_deb => true,
+        comment     => 'Debian experimental',
+        location    => 'http://http.debian.net/debian/',
+        release     => 'experimental',
+        repos       => 'main contrib non-free',
+        include_src => true,
+        include_deb => true,
     }
 }
--- czchen_environment/manifests/pkg/apt/dropbox.pp
+++ czchen_environment/manifests/pkg/apt/dropbox.pp
@@ -1,10 +1,10 @@
 class czchen_environment::pkg::apt::dropbox {
     apt::source { 'dropbox':
-      location    => 'http://linux.dropbox.com/debian',
-      release     => 'sid',
-      repos       => 'main',
-      include_src => false,
-      include_deb => true,
+        location    => 'http://linux.dropbox.com/debian',
+        release     => 'sid',
+        repos       => 'main',
+        include_src => false,
+        include_deb => true,
     }
 
     apt_key { 'dropbox':
