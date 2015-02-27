55363ffb56e970f4724f4f73fa7c80d65039e22b
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-02-28 03:44:46 +0800

    Add czchen_environment::cfg::udev

--- czchen_environment/manifests/cfg.pp
+++ czchen_environment/manifests/cfg.pp
@@ -1,11 +1,4 @@
 class czchen_environment::cfg {
-    file { '/etc/udev/rules.d/99-android.rules':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        ensure  => file,
-        content => 'SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", TAG+="uaccess"',
-    }
-
     include czchen_environment::cfg::sudo
+    include czchen_environment::cfg::udev
 }
