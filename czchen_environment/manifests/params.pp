edbbb587e151703fa99ba14d7dbfd7f8edab8e53
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-02-28 00:01:10 +0800

    Add params.pp

---
+++ czchen_environment/manifests/params.pp
@@ -0,0 +1,9 @@
+class czchen_environment::params {
+    $user  = 'czchen'
+    $home  = "/home/${user}"
+    $shell = '/bin/zsh'
+
+    $group = $user
+    $umask = 077
+    $path  = '/usr/local/bin:/usr/bin:/bin'
+}
--- czchen_environment/manifests/pkg.pp
+++ czchen_environment/manifests/pkg.pp
@@ -1,3 +1,5 @@
 class czchen_environment::pkg {
+    include czchen_environment::params
+
     include czchen_environment::pkg::apt
 }
