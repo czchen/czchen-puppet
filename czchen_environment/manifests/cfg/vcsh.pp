5e83589701a6c635016726094ca4326d2e2ae501
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-02-28 21:07:04 +0800

    Sync latest vcsh from repository
    Closed #6

--- czchen_environment/manifests/cfg/vcsh.pp
+++ czchen_environment/manifests/cfg/vcsh.pp
@@ -1,6 +1,6 @@
 define resource_vcsh($url) {
-    # FIXME: Run `vcsh pull` after all repositories are set.
-    exec { "vcsh ${name}":
+    # FIXME: Put check vcsh status into facts
+    exec { "vcsh clone ${name}":
         path        => '/bin:/usr/bin',
         user        => $czchen_environment::params::user::account,
         command     => "vcsh clone ${url} ${name}",
@@ -8,6 +8,17 @@ define resource_vcsh($url) {
             "HOME=${czchen_environment::params::user::home}",
         ],
         onlyif      => "vcsh status ${name} 2>&1|grep 'no repository found for'",
+        require     => Package['vcsh'],
+    }
+
+    exec { "vcsh pull ${name}":
+        path        => '/bin:/usr/bin',
+        user        => $czchen_environment::params::user::account,
+        command     => "vcsh pull ${name}",
+        environment => [
+            "HOME=${czchen_environment::params::user::home}",
+        ],
+        unless      => "vcsh status ${name} 2>&1|grep 'no repository found for'",
         require     => Package['vcsh'],
     }
 }
