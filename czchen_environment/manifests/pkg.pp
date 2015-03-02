7b2c378b5fbc7bf525b9a7c7f9662950dc72dc0e
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-03-02 23:09:06 +0800

    Fix missing include

--- czchen_environment/manifests/pkg.pp
+++ czchen_environment/manifests/pkg.pp
@@ -1,5 +1,6 @@
 class czchen_environment::pkg {
     include czchen_environment::pkg::apt
+    include czchen_environment::pkg::gem
     include czchen_environment::pkg::npm
     include czchen_environment::pkg::pip
 }
