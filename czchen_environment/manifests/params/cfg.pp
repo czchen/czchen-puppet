a8e79473c0562a5778b0238722c061eacf5d014f
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-05-22 22:12:07 +0800

    Add sbt configuration

--- czchen_environment/manifests/params/cfg.pp
+++ czchen_environment/manifests/params/cfg.pp
@@ -15,6 +15,7 @@ class czchen_environment::params::cfg {
         'python'         => { url => 'https://github.com/czchen/python.vcsh', },
         'rime'           => { url => 'https://github.com/czchen/rime.vcsh', },
         'rpm'            => { url => 'https://github.com/czchen/rpm.vcsh', } ,
+        'sbt'            => { url => 'https://github.com/czchen/sbt.vcsh', },
         'ssh'            => { url => 'https://github.com/czchen/ssh.vcsh', } ,
         'sublime-text-3' => { url => 'https://github.com/czchen/sublime-text-3.vcsh', },
         'taskwarrior'    => { url => 'https://github.com/czchen/taskwarrior.vcsh', },
