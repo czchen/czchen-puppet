a119e09b688b7d1c45f9f3e04778797b1d0b527d
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-08-12 23:11:39 +0800

    Disable chewing PPA due to GCC 5 transition

--- czchen_environment/manifests/pkg/apt.pp
+++ czchen_environment/manifests/pkg/apt.pp
@@ -7,7 +7,7 @@ class czchen_environment::pkg::apt {
 
             include czchen_environment::pkg::apt::atom
             include czchen_environment::pkg::apt::charlesproxy
-            include czchen_environment::pkg::apt::chewing
+            # include czchen_environment::pkg::apt::chewing
             include czchen_environment::pkg::apt::dropbox
             include czchen_environment::pkg::apt::fingerprint
             include czchen_environment::pkg::apt::firefox
