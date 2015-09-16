f6e44cfea8c9f8da7abb49612554eada7ff0a9e6
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-09-16 22:43:08 +0800

    Update vcsh repository info

--- czchen_environment/manifests/params/cfg.pp
+++ czchen_environment/manifests/params/cfg.pp
@@ -1,15 +1,12 @@
 class czchen_environment::params::cfg {
     $vcsh = {
         'atom'           => { url => 'https://github.com/czchen/atom.vcsh', },
-        'bzr'            => { url => 'https://github.com/czchen/bzr.vcsh', },
         'debian'         => { url => 'https://github.com/czchen/debian.vcsh', },
         'font'           => { url => 'https://github.com/czchen/font.vcsh', },
         'gdb'            => { url => 'https://github.com/czchen/gdb.vcsh', },
         'gem'            => { url => 'https://github.com/czchen/gem.vcsh', },
-        'git'            => { url => 'https://github.com/czchen/git.vcsh', },
         'golang'         => { url => 'https://github.com/czchen/golang.vcsh', },
         'gpg'            => { url => 'https://github.com/czchen/gpg.vcsh', },
-        'hg'             => { url => 'https://github.com/czchen/hg.vcsh', },
         'mutt'           => { url => 'https://github.com/czchen/mutt.vcsh', },
         'npm'            => { url => 'https://github.com/czchen/npm.vcsh', },
         'python'         => { url => 'https://github.com/czchen/python.vcsh', },
@@ -21,7 +18,7 @@ class czchen_environment::params::cfg {
         'syncthing'      => { url => 'https://github.com/czchen/syncthing.vcsh', },
         'taskwarrior'    => { url => 'https://github.com/czchen/taskwarrior.vcsh', },
         'tmux'           => { url => 'https://github.com/czchen/tmux.vcsh', },
-        'vcsh'           => { url => 'https://github.com/czchen/vcsh.vcsh', },
+        'vcs'            => { url => 'https://github.com/czchen/vcs.vcsh', },
         'vim'            => { url => 'https://github.com/czchen/vim.vcsh', },
         'zsh'            => { url => 'https://github.com/czchen/zsh.vcsh', },
     }
