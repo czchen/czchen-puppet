e832ff060899a20f1691be735ea8e96976190fe0
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-09-20 00:21:06 +0800

    Update vcsh repository

--- czchen_environment/manifests/params/cfg.pp
+++ czchen_environment/manifests/params/cfg.pp
@@ -1,25 +1,5 @@
 class czchen_environment::params::cfg {
     $vcsh = {
-        'atom'           => { url => 'https://github.com/czchen/atom.vcsh', },
-        'debian'         => { url => 'https://github.com/czchen/debian.vcsh', },
-        'font'           => { url => 'https://github.com/czchen/font.vcsh', },
-        'gdb'            => { url => 'https://github.com/czchen/gdb.vcsh', },
-        'gem'            => { url => 'https://github.com/czchen/gem.vcsh', },
-        'golang'         => { url => 'https://github.com/czchen/golang.vcsh', },
-        'gpg'            => { url => 'https://github.com/czchen/gpg.vcsh', },
-        'mutt'           => { url => 'https://github.com/czchen/mutt.vcsh', },
-        'npm'            => { url => 'https://github.com/czchen/npm.vcsh', },
-        'python'         => { url => 'https://github.com/czchen/python.vcsh', },
-        'rime'           => { url => 'https://github.com/czchen/rime.vcsh', },
-        'rpm'            => { url => 'https://github.com/czchen/rpm.vcsh', } ,
-        'sbt'            => { url => 'https://github.com/czchen/sbt.vcsh', },
-        'ssh'            => { url => 'https://github.com/czchen/ssh.vcsh', } ,
-        'sublime-text-3' => { url => 'https://github.com/czchen/sublime-text-3.vcsh', },
-        'syncthing'      => { url => 'https://github.com/czchen/syncthing.vcsh', },
-        'taskwarrior'    => { url => 'https://github.com/czchen/taskwarrior.vcsh', },
-        'tmux'           => { url => 'https://github.com/czchen/tmux.vcsh', },
-        'vcs'            => { url => 'https://github.com/czchen/vcs.vcsh', },
-        'vim'            => { url => 'https://github.com/czchen/vim.vcsh', },
-        'zsh'            => { url => 'https://github.com/czchen/zsh.vcsh', },
+        'config'           => { url => 'https://github.com/czchen/config', },
     }
 }
