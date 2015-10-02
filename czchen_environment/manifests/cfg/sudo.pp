b0a5ec64a61f94ed9f2126d6db900b8b6ec67a45
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-10-03 01:38:39 +0800

    Do not keep $HOME when sudo

--- czchen_environment/manifests/cfg/sudo.pp
+++ czchen_environment/manifests/cfg/sudo.pp
@@ -6,12 +6,4 @@ class czchen_environment::cfg::sudo {
         ensure  => file,
         content => "Defaults umask = 0022\nDefaults umask_override\n",
     }
-
-    file { '/etc/sudoers.d/home':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0400',
-        ensure  => file,
-        content => "Defaults env_keep += HOME\n",
-    }
 }
