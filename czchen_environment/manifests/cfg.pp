53eea8870dc8a5cc91c4f7ff4fe33a627bdb04a2
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-10-21 10:18:07 +0800

    Remove vcsh related configuration

--- czchen_environment/manifests/cfg.pp
+++ czchen_environment/manifests/cfg.pp
@@ -2,5 +2,4 @@ class czchen_environment::cfg {
     include czchen_environment::cfg::sudo
     include czchen_environment::cfg::sysrq
     include czchen_environment::cfg::udev
-    include czchen_environment::cfg::vcsh
 }
