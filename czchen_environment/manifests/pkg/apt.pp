ae4cb6df30ecc5bc8df52ad0ef834e9b69b2460d
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-12-20 21:12:53 +0800

    Disable unused Debian repository

--- czchen_environment/manifests/pkg/apt.pp
+++ czchen_environment/manifests/pkg/apt.pp
@@ -10,14 +10,14 @@ class czchen_environment::pkg::apt {
             # include czchen_environment::pkg::apt::chewing
             include czchen_environment::pkg::apt::dropbox
             # include czchen_environment::pkg::apt::fingerprint
-            include czchen_environment::pkg::apt::firefox
+            # include czchen_environment::pkg::apt::firefox
             include czchen_environment::pkg::apt::google
             include czchen_environment::pkg::apt::grafana
             include czchen_environment::pkg::apt::heroku
             include czchen_environment::pkg::apt::hipchat
-            include czchen_environment::pkg::apt::icinga
+            # include czchen_environment::pkg::apt::icinga
             include czchen_environment::pkg::apt::mongodb
-            include czchen_environment::pkg::apt::sbt
+            # include czchen_environment::pkg::apt::sbt
             include czchen_environment::pkg::apt::simplexsolutionsinc
             include czchen_environment::pkg::apt::sublimetext
             include czchen_environment::pkg::apt::syncthing
